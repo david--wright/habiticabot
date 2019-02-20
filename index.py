aca52abb4c3095b3ab24a7a9a72fe50ff3118355
David Wright <davidwright@utah.gov>
2019-02-20 16:58:33 -0700

    Finish out _addGroupUser

--- index.py
+++ index.py
@@ -4,20 +4,41 @@ import requests
 def _addGroupUser(group, user):
   client = boto3.resource('dynamodb')
   regTable = client.Table(DYNAMO_REG_TABLE)
+  groupTable = client.Table(DYNAMO_GROUP_TABLE)
+  botTable = client.Table(DYNAMO_BOT_TABLE)
+  status=False
+  try:
+    response = botTable.get_item(
+      Key={
+        'name': 'id'
+          }
+      )
+    botId=response['Item']
+  except ClientError as e:
+    return (status, e.response['Error']['Message'])
   try:
     response = regTable.get_item(
       Key={
         'user': user
           }
       )
-  except ClientError as e:
-    return (e.response['Error']['Message'])
+  except ClientError:
+    userData = None
   else:
-    userData = response['Item']
-#TODO: Check if user is registered and if so add them to group. Otherwise
-# return instructions for registering
+    if 'Item' in response:
+      userData = response['Item']
+    else: 
+      userData = None
+  if userData:
+    result = groupTable.put_item(Item= {'group':  group, 'user': user})
 
-  # table.put_item(Item= {'Group':  group, 'user': group})
+    status = True
+  else:
+    message = "Unable to add User to group. Please have the user register with the bot."
+    status=False
+    _kickGroupUser(group, user)
+  _sendTelegramMessage(message, group, botId)
+  return (status, result)
 
 def _command_start(message):
   pass
@@ -37,14 +58,24 @@ def _getTaskData(userId, userKey, task_type='dailys'):
   r = requests.get("https://habitica.com/api/v3/tasks/user",params=payload,headers=headers)
   return r
 
+def _kickGroupUser(group, user):
+  pass
+
 def _parseBotCommands(message):
   if 'text' in message:
-    methodCall = '_command_'+message['text'].split()[0][1:]
+    if len(message['text']) > 1:
+      spiltCommand = message['text'].split()
+      command = splitCommand[0][1:]
+      options = splitCommand[1:]
+    else:
+      command = message['text'][1:]
+      options = []
+    methodResult = getattr(self, '_command_'+command)(options)
   elif 'new_chat_member' in message:
-    methodCall = _addGroupUser
+    methodResult = _addGroupUser
   elif 'left_chat_member' in message:
     methodCall = _deleteGroupUser
-  return methodCall()
+  return methodResult
 
 def _sendTelegramMessage(message, group, botId):
   payload = {'chat_id': group, 'text': message}
